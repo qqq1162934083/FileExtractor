1502d6398769af1b355c5769c07b42713c5b0826
刘华栋 <1162934083@qq.com>
2022-09-12 21:00:46 +0800

    增加启动窗体

---
+++ FileExtractor/LaunchWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using FileExtractor.Libs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FileExtractor
+{
+    /// <summary>
+    /// LaunchWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class LaunchWindow : Window
+    {
+        public LaunchWindow()
+        {
+            InitializeComponent(); 
+            this.MouseLeftButtonDown += MainWindow_MouseLeftButtonDown;
+        }
+
+        void MainWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Grid || e.OriginalSource is Border || e.OriginalSource is Window)
+            {
+                WindowInteropHelper wih = new WindowInteropHelper(this);
+                Win32.SendMessage(wih.Handle, Win32.WM_NCLBUTTONDOWN, (int)Win32.HitTest.HTCAPTION, 0);
+                return;
+            }
+        }
+    }
+}
---
+++ FileExtractor/Libs/Win32.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExtractor.Libs
+{
+    class Win32
+    {
+        // Posted when the user presses the left mouse button while the cursor is within the nonclient area of a window  
+        public const int WM_NCLBUTTONDOWN = 0x00A1;
+
+        // Sends the specified message to a window or windows  
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+
+
+        public enum HitTest : int
+        {
+            /// <summary>  
+            /// On the screen background or on a dividing line between windows (same as HTNOWHERE, except that the DefWindowProc function produces a system beep to indicate an error).  
+            /// </summary>  
+            HTERROR = -2,
+
+            /// <summary>  
+            /// In a window currently covered by another window in the same thread (the message will be sent to underlying windows in the same thread until one of them returns a code that is not HTTRANSPARENT).  
+            /// </summary>  
+            HTTRANSPARENT = -1,
+
+            /// <summary>  
+            /// On the screen background or on a dividing line between windows.  
+            /// </summary>  
+            HTNOWHERE = 0,
+
+            /// <summary>  
+            /// In a client area.  
+            /// </summary>  
+            HTCLIENT = 1,
+
+            /// <summary>  
+            /// In a title bar.  
+            /// </summary>  
+            HTCAPTION = 2,
+
+            /// <summary>  
+            /// In a window menu or in a Close button in a child window.  
+            /// </summary>  
+            HTSYSMENU = 3,
+
+            /// <summary>  
+            /// In a size box (same as HTSIZE).  
+            /// </summary>  
+            HTGROWBOX = 4,
+
+            /// <summary>  
+            /// In a size box (same as HTGROWBOX).  
+            /// </summary>  
+            HTSIZE = 4,
+
+            /// <summary>  
+            /// In a menu.  
+            /// </summary>  
+            HTMENU = 5,
+
+            /// <summary>  
+            /// In a horizontal scroll bar.  
+            /// </summary>  
+            HTHSCROLL = 6,
+
+            /// <summary>  
+            /// In the vertical scroll bar.  
+            /// </summary>  
+            HTVSCROLL = 7,
+
+            /// <summary>  
+            /// In a Minimize button.  
+            /// </summary>  
+            HTMINBUTTON = 8,
+
+            /// <summary>  
+            /// In a Minimize button.  
+            /// </summary>  
+            HTREDUCE = 8,
+
+            /// <summary>  
+            /// In a Maximize button.  
+            /// </summary>  
+            HTMAXBUTTON = 9,
+
+            /// <summary>  
+            /// In a Maximize button.  
+            /// </summary>  
+            HTZOOM = 9,
+
+            /// <summary>  
+            /// In the left border of a resizable window (the user can click the mouse to resize the window horizontally).  
+            /// </summary>  
+            HTLEFT = 10,
+
+            /// <summary>  
+            /// In the right border of a resizable window (the user can click the mouse to resize the window horizontally).  
+            /// </summary>  
+            HTRIGHT = 11,
+
+            /// <summary>  
+            /// In the upper-horizontal border of a window.  
+            /// </summary>  
+            HTTOP = 12,
+
+            /// <summary>  
+            /// In the upper-left corner of a window border.  
+            /// </summary>  
+            HTTOPLEFT = 13,
+
+            /// <summary>  
+            /// In the upper-right corner of a window border.  
+            /// </summary>  
+            HTTOPRIGHT = 14,
+
+            /// <summary>  
+            /// In the lower-horizontal border of a resizable window (the user can click the mouse to resize the window vertically).  
+            /// </summary>  
+            HTBOTTOM = 15,
+
+            /// <summary>  
+            /// In the lower-left corner of a border of a resizable window (the user can click the mouse to resize the window diagonally).  
+            /// </summary>  
+            HTBOTTOMLEFT = 16,
+
+            /// <summary>  
+            /// In the lower-right corner of a border of a resizable window (the user can click the mouse to resize the window diagonally).  
+            /// </summary>  
+            HTBOTTOMRIGHT = 17,
+
+            /// <summary>  
+            /// In the border of a window that does not have a sizing border.  
+            /// </summary>  
+            HTBORDER = 18,
+
+            /// <summary>  
+            /// In a Close button.  
+            /// </summary>  
+            HTCLOSE = 20,
+
+            /// <summary>  
+            /// In a Help button.  
+            /// </summary>  
+            HTHELP = 21,
+        };
+    }
+
+}
