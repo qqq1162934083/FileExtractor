53acd3fcb9f0e4cd08f15c151613412831723405
刘华栋 <1162934083@qq.com>
2022-09-14 11:44:57 +0800

    tmp

---
+++ FileExtractor/WpfControls/MyTextBox.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileExtractor.WpfControls
+{
+    /// <summary>
+    /// MyTextBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyTextBox : TextBox
+    {
+        public MyTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
