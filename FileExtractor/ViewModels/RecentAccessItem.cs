2475592ea25cf276a4f01ffc1fda5caea95e2cc7
刘华栋 <1162934083@qq.com>
2022-09-16 01:35:17 +0800

    listBox编写

---
+++ FileExtractor/ViewModels/RecentAccessItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExtractor.ViewModels
+{
+    public class RecentAccessItem
+    {
+        public string FileName { get; set; }
+        public string DirPath { get; set; }
+        public DateTime AccessTime { get; set; }
+    }
+}
