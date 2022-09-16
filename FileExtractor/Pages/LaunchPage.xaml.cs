db5be3ea66902b3c92e0dccde1a72aa42db21738
刘华栋 <1162934083@qq.com>
2022-09-16 18:13:37 +0800

    ScrollBar tmp

--- FileExtractor/Pages/LaunchPage.xaml.cs
+++ FileExtractor/Pages/LaunchPage.xaml.cs
@@ -4,6 +4,7 @@ using NetCore5WpfToolsApp.Utils.Controls;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -66,6 +67,33 @@ namespace FileExtractor.Pages
         public void ApplyDataCache()
         {
             Console.WriteLine();
+        }
+    }
+    public class ScrollBarVisibilityConverter : IValueConverter
+    {
+        //public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //    var visibility = (ScrollBarVisibility)values.First(x => x is ScrollBarVisibility);
+        //    var isMouseOver = (bool)values.First(x => x is bool);
+
+        //    if (isMouseOver || visibility == ScrollBarVisibility.Visible) return Visibility.Visible;
+        //    else return visibility;
+        //}
+
+        //public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isMouseOver = (bool)value;
+            return isMouseOver ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
