e78dc0bdc87d6b4ea9b6c1ceb2e3bcf9f25ec030
刘华栋 <1162934083@qq.com>
2022-09-19 10:12:35 +0800

    MyButton2样式迁移到类

---
+++ FileExtractor/WpfControls/MyButton2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FileExtractor.WpfControls
+{
+    public class MyButton2 : Button
+    {
+    }
+}
